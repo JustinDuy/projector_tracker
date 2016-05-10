64bd85da0069a29a2bf752fd18d5e05602e403e2
Duy Nguyen <judichi87@gmail.com>
2016-05-10 15:33:52 +0200

    add CameraProjectorInterface

--- include/projector_tracker/CameraProjectorInterface.h
+++ include/projector_tracker/CameraProjectorInterface.h
@@ -3,7 +3,8 @@
 #define CAMERAPROJECTORINTERFACE_H
 
 #include <opencv2/core/core.hpp>
-
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 /**
  * @brief Allows synchronized projection of a pattern and acquisition through a camera.
  * 
@@ -15,7 +16,7 @@ public:
      * @brief Constructs a CameraProjectorInterface, identifying the target screen and cameras.
      * 
      */
-    CameraProjectorInterface(const cv::Mat& camera_intrinsics, const cv::Mat& projector_intrinsics);
+    CameraProjectorInterface(const cv::Mat& camera_intrinsics, size_t w, size_t h, const cv::Mat& projector_intrinsics, int capture_device);
     /**
      * @brief Releases the acquired resources.
      * 
@@ -23,12 +24,15 @@ public:
     ~CameraProjectorInterface();
     
     
-    cv::Mat projectAndAcquire(const cv::Mat& target_image);
+    cv::Mat projectAndAcquire(const cv::Mat& target_image, int delay, int seq);
     std::vector<cv::Mat> projectAndAcquire(const std::vector<cv::Mat>& target_images);
     
 protected:
     cv::Mat camera_intrinsics;  /// intrinsic RGB camera calibration parameters
     cv::Mat projector_intrinsics;  /// intrinsic projector calibration parameters
+    cv::VideoCapture capture; //Camera Handle
+    size_t camera_width;
+    size_t camera_height;
 };
 
 #endif // PROJECTORINTERFACE_H
--- nodes/node.cpp
+++ nodes/node.cpp
@@ -1,16 +1,9 @@
 #include <projector_tracker/ProjectorTracker.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <projector_tracker/CameraProjectorInterface.h>
 #include <vector>
-//#include <ros/ros.h>
-//#include <pcl_ros/point_cloud.h>
-//#include <sensor_msgs/PointCloud2.h>
-//#include "rgbprocess.h"
-
 #include <iostream>
 using namespace cv;
 using namespace std;
-void capturePatterns (VideoCapture &capture, const vector<Mat> &pattern, vector<Mat> &captured_patterns);
 int main (int argc, char **argv) {
     //load Kinect calibration file
     FileStorage fs ("../data/rgb_A00363813595051A.yaml", FileStorage::READ);
@@ -48,86 +41,11 @@ int main (int argc, char **argv) {
 
     ProjectorTracker tracker (cam1intrinsics, prointrinsics) ;
     vector<Mat> generated_patterns = tracker.getPattern (CAMERA_WIDTH, CAMERA_HEIGHT);
-
-    //namedWindow( "cam1", WINDOW_NORMAL );
-    //resizeWindow( "cam1", 640, 480 );
-
-    // Setting pattern window on second monitor (the projector's one)
-    namedWindow ("Pattern Window", WINDOW_NORMAL);
-    resizeWindow ("Pattern Window", CAMERA_WIDTH * 2, CAMERA_HEIGHT * 2);
-    moveWindow ("Pattern Window", CAMERA_WIDTH + 1000, 50);
-    //setWindowProperty( "Pattern Window", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN );
-#ifndef ___DEBUG
-
-    //namedWindow( "cam1", WINDOW_NORMAL );
-    //resizeWindow( "cam1", 640, 480 );
-    // Open kinect rgb cam, using openni
-    VideoCapture capture (CV_CAP_OPENNI);
-
-    if (!capture.isOpened()) {
-        // check if cam opened
-        cout << "kinect not opened!" << endl;
-        return -1;
-    }
-
-#endif
-
+    CameraProjectorInterface pro_cam_handler(cam1intrinsics, CAMERA_WIDTH, CAMERA_HEIGHT, cam1intrinsics, CV_CAP_OPENNI);
+    vector<Mat> captured_patterns = pro_cam_handler.projectAndAcquire(generated_patterns);
     //camera-base is known and assume scale factor is known
     //estimate Ra, ta: using graycode
-    Mat Ra, ta;
-    vector<Mat> captured_patterns;
+    Mat pro_cam_extrinsic = tracker.computeRelativePosition (captured_patterns);
 
-
-    //call synchronizer to project and capture graycode patterns
-    vector<Mat> capture_patterns;
-    capturePatterns (capture, generated_patterns, capture_patterns);
-
-
-    tracker.computeRelativePosition (captured_patterns);
-    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
-}
-void capturePatterns (VideoCapture &capture, const vector<Mat> &pattern, vector<Mat> &captured_patterns) {
-    String captured_path = "../captured/";
-    int i = 0;
-
-    while (i < pattern.size()) {
-        cout << "Waiting to save image number " << i + 1 << endl ;
-        imshow ("Pattern Window", pattern[i]);
-
-        if (i == 0)
-            waitKey (500);
-        else
-            waitKey (250);
-
-        Mat frame1;
-        capture.grab();
-        capture.retrieve (frame1, CV_CAP_OPENNI_BGR_IMAGE);
-
-        if (frame1.data) {
-            Mat gray;
-
-            // Resizing images to avoid issues for high resolution images, visualizing them as grayscale
-            resize (frame1, gray, Size (640, 480));
-            cvtColor (gray, gray, COLOR_RGB2GRAY);
-            bool save1 = false;
-
-            ostringstream name;
-            name << i + 1;
-            save1 = imwrite (captured_path + "im" + name.str() + ".png", frame1);
-
-            if (save1) {
-                cout << "pattern camera image number " << i + 1 << " saved" << endl ;
-            } else {
-                cout << "pattern cam1 images number " << i + 1 << " NOT saved" << endl << endl << "Retry, check the path" << endl << endl;
-            }
-
-            i++;
-        } else {
-            cout << "No frame data, waiting for new frame" << endl;
-        }
-    }
-
-    //end while loop for capturing patterns
-
 }
