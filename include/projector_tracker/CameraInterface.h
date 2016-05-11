977bd0f0e95262b05bec1b581382ed28708ae8fb
Marco Esposito <marco.esposito@tum.de>
2016-05-11 19:04:13 +0200

    Rewrote Camera Interface with some functionality

--- include/projector_tracker/CameraInterface.h
+++ include/projector_tracker/CameraInterface.h
@@ -1,34 +1,40 @@
 
 #ifndef CAMERAINTERFACE_H
 #define CAMERAINTERFACE_H
-#include <string>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <string>
+
 /**
- * @brief Allows to project images on a projector at full screen.
+ * @brief Allows to capture images from a video camera
  * 
  */
 class CameraInterface
 {
 public:
     /**
-     * @brief Constructs a ProjectorInterface, identifying the target screen.
+     * @brief Constructs a CameraInterface, identifying the target screen.
      * 
      */
-    CameraInterface();
+    CameraInterface(std::string calibration_filepath , std::string tag, int device_number = 0);
     /**
      * @brief Releases the acquired resources.
      * 
      */
     ~CameraInterface();
-public:
-    bool loadIntrinsic(std::string file, std::string tag);
-    cv::Mat get();
-protected:
-    cv::Mat intrinsics;  /// intrinsic projector calibration parameters
     
-    // TODO: full-screen window?
+public:
+    bool loadIntrinsics(std::string file, std::string tag);
+    cv::Mat getIntrinsics();
+    cv::Mat grabFrame();
+    
+protected:
+    cv::Mat intrinsics;  /// intrinsic camera calibration parameters
+    cv::VideoCapture capture; // Camera Handle
+    unsigned int width;
+    unsigned int height;
 };
 
 #endif // CAMERAINTERFACE_H
--- src/CameraInterface.cpp
+++ src/CameraInterface.cpp
@@ -1,11 +1,25 @@
+
 #include "projector_tracker/CameraInterface.h"
 #include <iostream>
 
-CameraInterface::CameraInterface()
+CameraInterface::CameraInterface(std::string matrix_file, std::string tag, int device_number)
 {
-
+    
+    if( !loadIntrinsics(matrix_file, tag) )
+    {
+      std::cerr << "Error: could not load camera intrinsic calibration from file " << matrix_file << std::endl;
+    }
+    
+    capture = cv::VideoCapture(device_number);
+    if( !capture.isOpened() )
+    {
+      std::cerr << "Error: camera device is not opened!" << std::endl;
+    }
+    width = capture.get(CV_CAP_PROP_FRAME_WIDTH);
+    height = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
 }
-bool CameraInterface::loadIntrinsic(std::string matrix_file , std::string tag)
+
+bool CameraInterface::loadIntrinsics(std::string matrix_file , std::string tag)
 {
     cv::FileStorage fs( matrix_file, cv::FileStorage::READ );
     if( !fs.isOpened() )
@@ -17,9 +31,18 @@ bool CameraInterface::loadIntrinsic(std::string matrix_file , std::string tag)
     fs[tag] >> intrinsics;
     return true;
 }
-cv::Mat CameraInterface::get(){
+
+cv::Mat CameraInterface::grabFrame() {
+    cv::Mat frame;
+    capture >> frame;
+    return frame;
+}
+
+cv::Mat CameraInterface::getIntrinsics()
+{
     return cv::Mat(intrinsics);
 }
+
 CameraInterface::~CameraInterface()
 {
 
